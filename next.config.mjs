663a188cf679c2d2ca1417bd22538b06c0507b80
hazmiyaa <bahriandicky@gmail.com>
2025-05-11 13:37:26 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  images: {
+    domains: ["drive.google.com"],
+  },
+};
+
+export default nextConfig;
